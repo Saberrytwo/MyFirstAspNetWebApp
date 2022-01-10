a95ced4c8fefaab2c03199dc2dc2b2df191a0304
Saberrytwo <90337853+Saberrytwo@users.noreply.github.com>
2022-01-09 17:11:34 -0700

    Add project files.

---
+++ MyFirstAspNetWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,7 @@
+﻿$("#btnSend").click( function () {
+    alert("From: " + $('#txtFrom').val() +
+        ", Subject:  " + $('#txtSubject').val() +
+        ", Message: " + $('#txtMsg').val());
+    $("#olive").fadeToggle('slow');
+})
+
